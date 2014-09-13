21b2d78dac81c3b4aa163879ca952c328d6cdcc8
immibis <immibis@gmail.com>
2014-09-13 14:47:17 +1200

    TradeMe requests can be sent while logged in.

--- WhereToLive/trademe.py
+++ WhereToLive/trademe.py
@@ -1,7 +1,9 @@
-URLBASE = "http://api.tmsandbox.co.nz"
 CACHE_TIME = 3600 # in seconds
 CACHE_DIR = "trademecache"
 
+import trademe_key
+
+URLBASE = "https://api.tmsandbox.co.nz" if trademe_key.SANDBOX else "https://api.trademe.co.nz"
 import urllib2
 import os.path
 import time
@@ -9,9 +11,20 @@ import json
 
 def _geturl(path):
 	"""GETs the specified path (which should start with /) under URLBASE, and returns the response body."""
-	conn = urllib2.urlopen(URLBASE+path)
-	data = conn.read()
-	conn.close()
+	headers = {}
+	if trademe_key.OAUTH_TOKEN != "":
+		headers['Authorization'] = 'OAuth oauth_consumer_key="'+trademe_key.CONSUMER_KEY+'", oauth_token="'+trademe_key.OAUTH_TOKEN+'", oauth_signature_method="PLAINTEXT", oauth_signature="'+trademe_key.CONSUMER_SECRET+'&'+trademe_key.OAUTH_TOKEN_SECRET+'"'
+	else:
+		headers['Authorization'] = 'OAuth oauth_consumer_key="'+trademe_key.CONSUMER_KEY+'", oauth_signature_method="PLAINTEXT", oauth_signature="'+trademe_key.CONSUMER_SECRET+'&"'
+		
+	try:
+		request = urllib2.Request(URLBASE+path, None, headers)
+		conn = urllib2.urlopen(request)
+		data = conn.read()
+		conn.close()
+	except urllib2.HTTPError as e:
+		print(e.read())
+		raise
 	return data
 	
 def _checkcachefile(filename):
@@ -22,18 +35,24 @@ def _checkcachefile(filename):
 		return False
 	staleness = time.time() - os.path.getmtime(fullpath)
 	return staleness < CACHE_TIME
+
+def _readcachefile(filename):
+	with open(CACHE_DIR+"/"+filename, "r") as f:
+		return f.read()
+
+def _writecachefile(filename, data):
+	with open(CACHE_DIR+"/"+filename, "w") as f:
+		f.write(data)
 	
 def _getcachedurl(path, cachefilename):
 	"""Returns the content of the specified URL, cached in CACHE_DIR/cachefilename. If the cache file was
 	written recently, the URL will not be accessed."""
-	cachepath = CACHE_DIR + "/" + cachefilename
+	
 	if _checkcachefile(cachefilename):
-		with open(cachepath, "r") as f:
-			return f.read()
+		return _readcachefile(cachefilename)
 	
 	data = _geturl(path)
-	with open(cachepath, "w") as f:
-		f.write(data)
+	_writecachefile(cachefilename, data)
 	return data
 
 _locality_data = None
@@ -70,6 +89,44 @@ def get_localities():
 	if _locality_data is None:
 		_locality_data = json.loads(_getcachedurl("/v1/Localities.json", "Localities.json"))
 	return _locality_data
+
+_job_categories = None
+def get_job_categories():
+	"""
+	Returns the job categories list.
+	Example format:
+		[
+			{
+				"Name": "Accounting",
+				"Code": "5001",
+				"SubCategories": [
+					{
+						"Name": "Accountants",
+						"Code": "5002",
+						"SubCategories": []
+					},
+					... other sub-categories...
+				]
+			},
+			... other top-level categories...
+		]
+	Note: Subcategories can have subsubcategories and so on, but currently none do.
+	"""
+	global _job_categories
+	if _job_categories is None:
+		_job_categories = json.loads(_getcachedurl("/v1/Categories/Jobs.json", "JobCategories.json"))
+		
+		# Change 'SubCategories': None to 'SubCategories': []
+		def fix_subcategories(category):
+			if category['SubCategories'] == None:
+				category['SubCategories'] = []
+			else:
+				for subcat in category['SubCategories']:
+					fix_subcategories(subcat)
+		for category in _job_categories:
+			fix_subcategories(category)
+		
+	return _job_categories
 	
 
-print(repr(get_localities()))
+print(repr(get_job_categories()))
